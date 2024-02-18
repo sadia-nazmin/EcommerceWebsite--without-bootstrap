df321c68f12a9f2365eba2a219edbf3f95d5865c
Sadia Nazmin <sadianazmin228@gmail.com>
2024-02-19 05:01:38 +0600

    creating order-summary component & styling the UI. Adding checkout routing

--- client/src/app/basket/basket.module.ts
+++ client/src/app/basket/basket.module.ts
@@ -4,9 +4,16 @@ import { BasketComponent } from './basket.component';
 import { BasketRoutingModule } from './basket-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [BasketComponent],
-  imports: [CommonModule, BasketRoutingModule, MatTableModule, MatIconModule],
+  imports: [
+    CommonModule,
+    BasketRoutingModule,
+    MatTableModule,
+    MatIconModule,
+    SharedModule,
+  ],
 })
 export class BasketModule {}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent],
-  imports: [CommonModule, MatPaginatorModule],
-  exports: [MatPaginatorModule, PagingHeaderComponent, PagerComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderSummaryComponent],
+  imports: [CommonModule, MatPaginatorModule, MatDividerModule],
+  exports: [
+    MatPaginatorModule,
+    PagingHeaderComponent,
+    PagerComponent,
+    OrderSummaryComponent,
+  ],
 })
 export class SharedModule {}
