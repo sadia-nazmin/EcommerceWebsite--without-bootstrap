45518be8865571a7ec7713f7ad8ff5d5f75837ac
Sadia Nazmin <sadianazmin228@gmail.com>
2024-02-21 17:31:51 +0600

    customer-service page

--- client/src/app/customer-service/customer-service.component.ts
+++ client/src/app/customer-service/customer-service.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-customer-service',
   templateUrl: './customer-service.component.html',
-  styleUrls: ['./customer-service.component.scss']
+  styleUrls: ['./customer-service.component.scss'],
 })
 export class CustomerServiceComponent {
-
+  mail = 'sadianazmin228@gmail.com';
 }
